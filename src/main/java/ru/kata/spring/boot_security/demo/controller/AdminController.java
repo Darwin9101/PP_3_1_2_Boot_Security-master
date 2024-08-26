1a76983104fb2ea6cf00fe0d8f58b8c5a644bd70
Alexandr Ladygin <Darwin9101@gmail.com>
2024-08-26 14:21:52 +0300

    1

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,103 +1,102 @@
-package ru.kata.spring.boot_security.demo.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.models.Role;
-import ru.kata.spring.boot_security.demo.models.User;
-import ru.kata.spring.boot_security.demo.service.RoleService;
-import ru.kata.spring.boot_security.demo.service.RoleServiceInt;
-import ru.kata.spring.boot_security.demo.service.ServiceInt;
-import ru.kata.spring.boot_security.demo.service.UserService;
-
-import java.security.Principal;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-
-@Controller
-@RequestMapping("/admin")
-public class AdminController {
-
-    private final ServiceInt userService;
-    private final RoleServiceInt roleService;
-
-    @Autowired
-    public AdminController(UserService userService, RoleService roleService) {
-        this.userService = userService;
-        this.roleService = roleService;
-    }
-
-    //@PreAuthorize("hasRole('ADMIN')") не обязательна, в конфиге уже указал
-    @Transactional(readOnly = true)
-    @GetMapping
-    public String index(ModelMap model, Principal principal) {
-        List<User> users = userService.getAllUsers();
-        model.addAttribute("users", users);
-
-        if (principal != null) {
-            User user = userService.getUserByName(principal.getName());
-            model.addAttribute("loggedInAdmin", user);
-        }
-
-        return "admin";
-    }
-
-    @GetMapping("/create")
-    public String showAddUserForm(ModelMap model) {
-        model.addAttribute("user", new User());
-        Collection<Role> allRoles = roleService.getAllRoles();
-        model.addAttribute("allRoles", allRoles);
-        return "addUser"; // Страница для добавления пользователя
-    }
-
-    @PostMapping("/create")
-    @Transactional
-    public String createUser(@ModelAttribute User user) {
-        userService.insertUser(user);
-        return "redirect:/admin"; // Перенаправление на список пользователей
-    }
-
-    @Transactional(readOnly = true)
-    @GetMapping("/edit/{id}")
-    public String editUser(@PathVariable Long id, ModelMap model) {
-        User user = userService.getUserById(id);
-        model.addAttribute("user", user);
-        Collection<Role> allRoles = roleService.getAllRoles();
-        model.addAttribute("allRoles", allRoles);
-        return "editUser"; // Страница для редактирования пользователя
-    }
-
-    @PostMapping("/edit/{id}")
-    @Transactional
-    public String updateUser(@RequestParam String name,
-                             @RequestParam String email,
-                             @RequestParam(required = false) String password, // Пароль будет необязательным
-                             @RequestParam Set<Role> roles,
-                             @PathVariable Long id) {
-        userService.updateUser(name, email, password, roles, id); // Передайте параметры в сервис
-        return "redirect:/admin"; // Перенаправление после обновления
-    }
-
-    @PostMapping("/delete/{id}")
-    @Transactional
-    public String deleteUser(@PathVariable Long id) {
-        userService.deleteUser(id); // Измененный вызов
-        return "redirect:/admin"; // Перенаправление после удаления
-    }
-
-    @PostMapping("/logout")
-    public String logout() {
-        return "redirect:/login"; // Перенаправление на страницу логина после выхода
-    }
-
-    @Transactional(readOnly = true)
-    @GetMapping("/admin")
-    public String adminPage(ModelMap model, Principal principal) {
-        User user = userService.getUserByName(principal.getName());
-        model.addAttribute("loggedInAdmin", user);
-        return "admin"; // имя вашего шаблона
-    }
-}
+//package ru.kata.spring.boot_security.demo.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.transaction.annotation.Transactional;
+//import org.springframework.ui.ModelMap;
+//import org.springframework.web.bind.annotation.*;
+//import ru.kata.spring.boot_security.demo.models.Role;
+//import ru.kata.spring.boot_security.demo.models.User;
+//import ru.kata.spring.boot_security.demo.service.RoleService;
+//import ru.kata.spring.boot_security.demo.service.RoleServiceInt;
+//import ru.kata.spring.boot_security.demo.service.ServiceInt;
+//import ru.kata.spring.boot_security.demo.service.UserService;
+//
+//import java.security.Principal;
+//import java.util.Collection;
+//import java.util.List;
+//import java.util.Set;
+//
+//@Controller
+//@RequestMapping("/admin")
+//public class AdminController {
+//
+//    private final ServiceInt userService;
+//    private final RoleServiceInt roleService;
+//
+//    @Autowired
+//    public AdminController(UserService userService, RoleService roleService) {
+//        this.userService = userService;
+//        this.roleService = roleService;
+//    }
+//
+//    @Transactional(readOnly = true)
+//    @GetMapping
+//    public String index(ModelMap model, Principal principal) {
+//        List<User> users = userService.getAllUsers();
+//        model.addAttribute("users", users);
+//
+//        if (principal != null) {
+//            User user = userService.getUserByName(principal.getName());
+//            model.addAttribute("loggedInAdmin", user);
+//        }
+//
+//        return "admin";
+//    }
+//
+//    @GetMapping("/create")
+//    public String showAddUserForm(ModelMap model) {
+//        model.addAttribute("user", new User());
+//        Collection<Role> allRoles = roleService.getAllRoles();
+//        model.addAttribute("allRoles", allRoles);
+//        return "addUser"; // Страница для добавления пользователя
+//    }
+//
+//    @PostMapping("/create")
+//    @Transactional
+//    public String createUser(@ModelAttribute User user) {
+//        userService.insertUser(user);
+//        return "redirect:/admin"; // Перенаправление на список пользователей
+//    }
+//
+//    @Transactional(readOnly = true)
+//    @GetMapping("/edit/{id}")
+//    public String editUser(@PathVariable Long id, ModelMap model) {
+//        User user = userService.getUserById(id);
+//        model.addAttribute("user", user);
+//        Collection<Role> allRoles = roleService.getAllRoles();
+//        model.addAttribute("allRoles", allRoles);
+//        return "editUser"; // Страница для редактирования пользователя
+//    }
+//
+//    @PostMapping("/edit/{id}")
+//    @Transactional
+//    public String updateUser(@RequestParam String name,
+//                             @RequestParam String email,
+//                             @RequestParam(required = false) String password, // Пароль будет необязательным
+//                             @RequestParam Set<Role> roles,
+//                             @PathVariable Long id) {
+//        userService.updateUser(name, email, password, roles, id); // Передайте параметры в сервис
+//        return "redirect:/admin"; // Перенаправление после обновления
+//    }
+//
+//    @PostMapping("/delete/{id}")
+//    @Transactional
+//    public String deleteUser(@PathVariable Long id) {
+//        userService.deleteUser(id); // Измененный вызов
+//        return "redirect:/admin"; // Перенаправление после удаления
+//    }
+//
+//    @PostMapping("/logout")
+//    public String logout() {
+//        return "redirect:/login"; // Перенаправление на страницу логина после выхода
+//    }
+//
+//    @Transactional(readOnly = true)
+//    @GetMapping("/admin")
+//    public String adminPage(ModelMap model, Principal principal) {
+//        User user = userService.getUserByName(principal.getName());
+//        model.addAttribute("loggedInAdmin", user);
+//        return "admin"; // имя вашего шаблона
+//    }
+//}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminRestController.java
@@ -0,0 +1,61 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.service.ServiceInt;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminRestController {
+    private final ServiceInt userService;
+
+    @Autowired
+    public AdminRestController(ServiceInt userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        } else
+        {
+            return ResponseEntity.ok(user);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        userService.insertUser(user);
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        if (user != null) {
+            userService.updateUser(user.getName(),user.getEmail(),user.getPassword(),user.getRoles(), id);
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        if (userService.getUserById(id) != null) {
+            userService.deleteUser(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
